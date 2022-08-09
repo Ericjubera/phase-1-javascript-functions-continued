405f89dc3eb750bd9d4a1722c29eb86cee16bb60
eric jubera <ericjubera@gmail.com>
2022-08-09 11:19:11 -0500

    commit tree 9c066b3530b379957887954c0aed4b4ab70eb23f

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function saturdayFun(uska="roller-skate"){
+return`This Saturday, I want to ${uska}!`
+}
+function mondayWork(yes="go to the office"){
+    return`This Monday, I will ${yes}.`
+}
+function wrapAdjective(yes="*"){
+    return function(yessir="special"){
+        return `You are ${yes}${yessir}${yes}!`}
+    }
+
